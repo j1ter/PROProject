95674d2bee79983c207dfee61b45762ed3926ff8
Daniyal Samat <112614118+j1ter@users.noreply.github.com>
2023-04-22 12:37:44 +0600

    update
    update

---
+++ server/controllers/productController.js
@@ -0,0 +1,33 @@
+const uuid = require('uuid')
+const path = require('path');
+const {Product} = require('../models/models')
+const ApiError = require('../error/ApiError')
+
+class ProductController {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const product = await Product.create({name, price, brandId, typeId, img: fileName})
+
+            return res.json(product)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+
+    }
+
+    async getALL(req, res) {
+        
+    }
+
+    async getOne(req, res) {
+        
+    }
+
+}
+
+module.exports = new ProductController()
