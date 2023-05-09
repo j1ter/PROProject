95c2793897a3c2876a809d5d3abb33d0e906fc90
j1ter <daniyal.samat@narxoz.kz>
2023-05-09 12:54:24 +0600

    Front-end update
    v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-
-import './App.css';
-import Layout from './components/Layout/Layout';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import News from './pages/News';
+import Recycling from './pages/Recycling';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
     <>
-    <Layout>
-    <h1>Eco market App</h1>
-    </Layout>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/product' element={<ProductPage />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/news' element={<News />} />
+      <Route path='/recycling' element={<Recycling />} />
+      <Route path='/policy' element={<Policy />} />
+      <Route path='*' element={<Pagenotfound />} />
+    </Routes>
      
     </>
   );
---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const ProductPage = () => {
+  return (
+    <Layout>
+        <h1>Product Page</h1>
+    </Layout>
+  )
+}
+
+export default ProductPage
