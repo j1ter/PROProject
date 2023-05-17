508e0b9842dfb5e307d644f30bf3435ed381b031
Dishka123 <dilshat.ablikimov@narxoz.kz>
2023-05-17 17:10:23 +0600

    Update & Delete product
    v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,57 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import About from './pages/About';
-import News from './pages/News';
-import Recycling from './pages/Recycling';
-import Policy from './pages/Policy';
-import Pagenotfound from './pages/Pagenotfound';
-import ProductPage from './pages/ProductPage';
-import Contact from './pages/Contact';
-import Register from './pages/Auth/Register';
-import Login from './pages/Auth/Login';
-import Dashboard from './pages/user/Dashboard';
-import PrivateRoute from './components/Routes/Private';
-import ForgotPassword from './pages/Auth/ForgotPassword';
-import AdminRoute from './components/Routes/AdminRoute';
-import AdminDashboard from './pages/Admin/AdminDashboard';
-import CreateProduct from './pages/Admin/CreateProduct';
-import CreateCategory from './pages/Admin/CreateCategory';
-import Users from './pages/Admin/Users';
-import Orders from './pages/user/Orders';
-import Profile from './pages/user/Profile';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import News from "./pages/News";
+import Recycling from "./pages/Recycling";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
+import ProductPage from "./pages/ProductPage";
+import Contact from "./pages/Contact";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
+import PrivateRoute from "./components/Routes/Private";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
+import AdminRoute from "./components/Routes/AdminRoute";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import CreateProduct from "./pages/Admin/CreateProduct";
+import CreateCategory from "./pages/Admin/CreateCategory";
+import Users from "./pages/Admin/Users";
+import Orders from "./pages/user/Orders";
+import Profile from "./pages/user/Profile";
+import Products from "./pages/Admin/Products";
+import UpdateProduct from "./pages/Admin/UpdateProduct";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/dashboard' element={<PrivateRoute />}>
-        <Route path="user" element={<Dashboard />} />
-        <Route path="user/orders" element={<Orders />} />
-        <Route path="user/profile" element={<Profile />} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
+        </Route>
 
-      </Route>
-      <Route path='/dashboard' element={<AdminRoute />}>
-        <Route path='admin' element={<AdminDashboard />} />
-        <Route path='admin/create-category' element={<CreateCategory />} />
-        <Route path='admin/create-product' element={<CreateProduct />} />
-        <Route path='admin/users' element={<Users />} />
-      </Route>
-      
-      <Route path='/login' element={<Login />} />
-      <Route path='/forgot-password' element={<ForgotPassword />} />
-      <Route path='/product' element={<ProductPage />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/news' element={<News />} />
-      <Route path='/recycling' element={<Recycling />} />
-      <Route path='/policy' element={<Policy />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='*' element={<Pagenotfound />} />
-    </Routes>
-     
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/recycling" element={<Recycling />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
